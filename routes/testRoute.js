3b85886f678ffad6b50f56f1e22eb037bb9eb000
Courtney Annett <cannett@outlook.com>
2022-04-14 17:17:55 +0100

    reformatted for nodejs

---
+++ routes/testRoute.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('index.html', {
+    root: 'C:/Users/canne/SketchAI/views/'
+  });
+  //res.render('index.html', { title: 'Express' });
+});
+
+module.exports = router;
